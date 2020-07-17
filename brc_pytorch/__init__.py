57ffaf620b47858ec07bd31ff49be6dbd6947fbc
Jannis Born <jab@zurich.ibm.com>
2020-07-18 01:09:50 +0200

    chore: package housekeeping

--- brc_pytorch/__init__.py
+++ brc_pytorch/__init__.py
@@ -1,2 +1,2 @@
 name = 'brc_pytorch'
-__version__ = '0.0.1'
+__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='brc_pytorch',
-    version='0.0.1',
+    version='0.1',
     description=('Pytorch Implementation of BRC.'),
     long_description=open('README.md').read(),
     url='https://github.com/niklexical/brc_pytorch',
     author='Nikita Janakarajan, Jannis Born',
     author_email=('nikita.janakarajan907@gmail.com, jannis.born@gmx.de'),
-    install_requires=['numpy', 'pandas', 'scipy', 'torch'],
+    install_requires=['numpy', 'torch'],
     packages=find_packages('.'),
     zip_safe=False,
 )
