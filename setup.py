e13b25df4e5ca50db1968628a085ce1b817cbaf8
Nikita Janakarajan <NJA@zurich.ibm.com>
2020-07-17 17:20:52 +0200

    requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Install package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='brc_pytorch',
+    version='0.0.1',
+    description=('Pytorch Implementation of BRC.'),
+    long_description=open('README.md').read(),
+    url='https://github.com/niklexical/brc_pytorch',
+    author='Nikita Janakarajan, Jannis Born',
+    author_email=('nikita.janakarajan907@gmail.com,'),
+    install_requires=['numpy', 'pandas', 'scipy', 'torch'],
+    packages=find_packages('.'),
+    zip_safe=False,
+)
