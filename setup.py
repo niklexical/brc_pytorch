928f58410511778be51672b0da4c1b6cfda5dce4
Jannis Born <jab@zurich.ibm.com>
2020-07-18 02:55:28 +0200

    feat: PyPi package setup

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 """Install package."""
+import os
 from setuptools import setup, find_packages
+
+if os.path.exists('README.md'):
+    long_description = open('README.md').read()
+else:
+    long_description = '''Pytorch Implementation of Bistable Recurrent Cell.'''
 
 setup(
     name='brc_pytorch',
     version='0.1',
+    license='MIT',
     description=('Pytorch Implementation of BRC.'),
-    long_description=open('README.md').read(),
+    long_description=long_description,
     url='https://github.com/niklexical/brc_pytorch',
     author='Nikita Janakarajan, Jannis Born',
     author_email=('nikita.janakarajan907@gmail.com, jannis.born@gmx.de'),
     install_requires=['numpy', 'torch'],
+    keywords=['PyTorch', 'Deep Learning', 'RNN', 'BRC'],
     packages=find_packages('.'),
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
